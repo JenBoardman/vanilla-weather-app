b37e56da937bc702ada6a2f14c324d122b34835d
JenBoardman <harter.art.creations@gmail.com>
2023-04-02 18:23:12 +0100

    update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,47 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+  let dateNumber = date.getDate();
+  return `${day}, ${month} ${dateNumber}`;
+}
+
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 function displayTempreature(response) {
   let tempretureElement = document.querySelector("#currenttemp");
   tempretureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -9,6 +53,10 @@ function displayTempreature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatTime(response.data.time * 1000);
 }
 
 let apiKey = "0622tcaa31a9d02f3oa3ff0e63b0bb64";
